2e1b5aa7c4fb5514e75f406d3cdce7d4b09a65d9
Ryan Porter <ryan.m.porter@gmail.com>
2023-12-08 22:16:03 -0800

    Refactor solve script to accept multiple days

--- src/advent/__main__.py
+++ src/advent/__main__.py
@@ -2,6 +2,7 @@
 
 import argparse
 import importlib
+import time
 
 import advent
 
@@ -10,16 +11,24 @@ def main():
     """Solve the puzzle for the given day."""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("day", type=int, nargs=1)
+    parser.add_argument("days", type=int, nargs="*")
 
     args = parser.parse_args()
 
-    day = args.day[0]
-    day_name = "day_{:02d}".format(*args.day)
+    st = time.time()
 
-    module = importlib.import_module(day_name, advent)
+    for day in args.days:
+        day_name = "day_{:02d}".format(day)
 
-    advent.solve(day, getattr(module, "part_01", None), getattr(module, "part_02", None))
+        module = importlib.import_module(day_name, advent)
+
+        print(f"\nDay {day:02d}")
+        advent.solve(day, getattr(module, "part_01", None), getattr(module, "part_02", None))
+
+    et = time.time()
+
+    if len(args.days) > 1:
+        print(f"\nSolved {len(args.days)} puzzles in {et - st:.2f} seconds.")
 
 
 if __name__ == "__main__":
