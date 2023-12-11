f33da15266afccf484746eac86384446d898b3df
Ryan Porter <ryan.m.porter@gmail.com>
2023-12-10 17:20:01 -0800

    Day 10, Part 02 wip

--- src/advent/day_10.py
+++ src/advent/day_10.py
@@ -1,19 +1,46 @@
 """Advent of Code 2023, Day 10."""
 
 import collections
+import sys
 
-from typing import Dict, List, Tuple
+from typing import Dict, Iterator, List, Set, Tuple
 
 
 class Cell(collections.namedtuple("Cell", "x, y pipe", defaults=(0, 0, None))):
+    def __repr__(self):
+        if self.pipe:
+            return f"{self.__class__.__name__}(x={self.x}, y={self.y}, pipe={self.pipe})"
+        else:
+            return f"{self.__class__.__name__}(x={self.x}, y={self.y})"
+
     def __hash__(self):
         return hash((self.x, self.y))
+
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y
+
+    def __ge__(self, other):
+        return self.x >= other.x and self.y >= other.y
 
     def __eq__(self, other):
         if isinstance(other, Cell):
             return (self.x, self.y) == (other.x, other.y)
         else:
             return False
+
+    def adjacent(self) -> Iterator:
+        """Yield the cells adjacent to this one."""
+
+        yield Cell(self.x + 1, self.y + 1)
+        yield Cell(self.x + 1, self.y + 0)
+        yield Cell(self.x + 1, self.y + -1)
+
+        yield Cell(self.x + 0, self.y + 1)
+        yield Cell(self.x + 0, self.y + -1)
+
+        yield Cell(self.x - 1, self.y + 1)
+        yield Cell(self.x - 1, self.y + 0)
+        yield Cell(self.x - 1, self.y + -1)
 
 
 LAST_MOVE = {
@@ -43,46 +70,80 @@ PIPES = {
 def part_01(puzzle_input: List[str]) -> int:
     """Solve part one."""
 
-    start_at, pipes = parse(puzzle_input)
+    start_at, pipes, __ = parse(puzzle_input)
+    start_at, pipes = get_map(start_at, pipes)
 
-    a, __ = pipes[start_at]
+    first_move = PIPES[start_at.pipe][0]
 
-    cw = run(start_at, pipes, first_move=a)
+    loop = run(start_at, pipes, first_move)
 
-    if cw % 2 == 0:
-        return cw // 2
+    n = len(loop)
+
+    if n % 2 == 0:
+        return n // 2
     else:
-        return (cw + 1) // 2
+        return (n + 1) // 2
 
 
 def part_02(puzzle_input: List[str]) -> int:
     """Solve part two."""
 
+    # I have been trying to solve this for too many hours.
+    # I think it has something to do with a scanline search.
+    # There are sixteen possible turns following the loop clockwise"
+    # F-7 -> S
+    # F-J -> S
+    # L-J -> N
+    # L-7 -> N
+    # J-L => N
+    # J-F -> N
+    # 7-F -> S
+    # 7-L -> S
+    #
+    # F|L -> E
+    # F|J -> E
+    # 7|J -> W
+    # 7|L -> W
+    # L|F -> E
+    # L|7 -> E
+    # J|F -> W
+    # J|7 -> W
+    # For each straight tile between the turns,
+    # all tiles to "right" until the next part of the loop
+    # are bounded by the loop
 
-def parse(puzzle_input: List[str]) -> Dict[Cell, Dict[bool, Cell]]:
+
+def parse(puzzle_input: List[str]) -> List[Cell]:
     """Parse the puzzle input"""
 
-    result = {}
+    result = []
 
     start_at = None
 
     for y, line in enumerate(puzzle_input):
         for x, char in enumerate(line):
-            if char == ".":
-                continue
+            cell = Cell(x, y, char)
 
             if char == "S":
-                start_at = Cell(x, y, char)
-                continue
+                start_at = cell
 
-            if char in PIPES:
-                cell = Cell(x, y, char)
-                result[cell] = cell
+            result.append(cell)
 
-    tmp = dict(result)
-    tmp[start_at] = start_at
+    return start_at, result, Cell(x, y)
 
-    for cell in result:
+
+def get_map(start_at: Cell, pipes: List[Cell]) -> Dict[Cell, Dict[str, Cell]]:
+    result = {}
+
+    cells = {cell: cell for cell in pipes}
+
+    for cell in pipes:
+        if cell.pipe == "S":
+            continue
+
+        if cell.pipe == ".":
+            continue
+
         result[cell] = {}
 
         for direction in PIPES[cell.pipe]:
@@ -90,16 +151,21 @@ def parse(puzzle_input: List[str]) -> Dict[Cell, Dict[bool, Cell]]:
             dest = Cell(cell.x + move.x, cell.y + move.y)
 
             try:
-                result[cell][direction] = tmp[dest]
+                result[cell][direction] = cells[dest]
             except KeyError:
                 pass
 
     start_moves = {}
 
-    for cell, moves in result.items():
+    for cell, moves in sorted(result.items()):
         for direction, move in moves.items():
             if move == start_at:
                 start_moves[LAST_MOVE[direction]] = cell
+
+    for pipe, (a, b) in PIPES.items():
+        if a in start_moves and b in start_moves:
+            start_at = Cell(start_at.x, start_at.y, pipe)
+            break
 
     result[start_at] = start_moves
 
@@ -107,14 +173,16 @@ def parse(puzzle_input: List[str]) -> Dict[Cell, Dict[bool, Cell]]:
 
 
 def run(start_at: Cell, pipes: Dict[Cell, Dict[bool, Cell]], first_move: str) -> int:
-    """Return the number of steps in the loop."""
+    """Return the route of the loop."""
+
+    result = []
 
     prev = start_at
     curr = pipes[start_at][first_move]
 
-    n = 1
-
     while True:
+        result.append(prev)
+
         (move_a, next_a), (move_b, next_b) = pipes[curr].items()
 
         if next_a == prev:
@@ -125,8 +193,7 @@ def run(start_at: Cell, pipes: Dict[Cell, Dict[bool, Cell]], first_move: str) ->
             raise RuntimeError("Impossible")
 
         if curr == start_at:
+            result.append(prev)
             break
-        else:
-            n += 1
 
-    return n
+    return result
--- tests/test_day_10.py
+++ tests/test_day_10.py
@@ -16,9 +16,12 @@ def test_parse_example_01a():
         ".....",
     ]
 
-    start_at, result = advent.day_10.parse(puzzle_input)
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
 
     assert start_at == Cell(1, 1, "S")
+    assert mx == Cell(4, 4)
+
+    start_at, result = advent.day_10.get_map(start_at, result)
 
     check_results(start_at, result)
 
@@ -35,9 +38,12 @@ def test_parse_example_01b():
         "LJ...",
     ]
 
-    start_at, result = advent.day_10.parse(puzzle_input)
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
 
     assert start_at == Cell(0, 2, "S")
+    assert mx == Cell(4, 4)
+
+    start_at, result = advent.day_10.get_map(start_at, result)
 
     check_results(start_at, result)
 
@@ -82,3 +88,204 @@ def test_example_01b():
     answer = advent.day_10.part_01(puzzle_input)
 
     assert answer == 8
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        [Cell(1, 2), Cell(1, 2), True],
+        [Cell(1, 2), Cell(1, 3), True],
+        [Cell(1, 2), Cell(3, 2), True],
+        [Cell(1, 2), Cell(3, 3), True],
+        [Cell(1, 2), Cell(1, 1), False],
+        [Cell(1, 2), Cell(0, 1), False],
+        [Cell(2, 2), Cell(1, 2), False],
+    ],
+    ids=[
+        "ax == ax, ay == by",
+        "ax == ax, ay <  by",
+        "ax <  ax, ay == by",
+        "ax <  ax, ay <  by",
+        "ax == by, ay >  by",
+        "ax >  bx, ay >  by",
+        "ax >  bx, ay == by",
+    ],
+)
+def test_cell_less_than_or_equal(a, b, expected):
+    assert (a <= b) == expected
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        [Cell(1, 2), Cell(1, 2), True],
+        [Cell(1, 3), Cell(1, 2), True],
+        [Cell(2, 2), Cell(1, 2), True],
+        [Cell(2, 3), Cell(1, 2), True],
+        [Cell(1, 1), Cell(1, 2), False],
+        [Cell(0, 1), Cell(1, 2), False],
+        [Cell(0, 2), Cell(1, 2), False],
+    ],
+    ids=[
+        "ax == ax, ay == by",
+        "ax == ax, ay <  by",
+        "ax <  ax, ay == by",
+        "ax <  ax, ay <  by",
+        "ax == by, ay <  by",
+        "ax <  bx, ay <  by",
+        "ax <  bx, ay == by",
+    ],
+)
+def test_cell_geater_than_or_equal(a, b, expected):
+    assert (a >= b) == expected
+
+
+@pytest.mark.skip
+def test_example_02_01a():
+    puzzle_input = [
+        ".....",
+        ".S-7.",
+        ".|.|.",
+        ".L-J.",
+        ".....",
+    ]
+
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
+
+    cells = advent.day_10.get_enclosed_cells(start_at, result, mx)
+
+    assert cells == {
+        Cell(2, 2),
+    }
+
+
+@pytest.mark.skip
+def test_example_02_01b():
+    puzzle_input = [
+        "..F7.",
+        ".FJ|.",
+        "SJ.L7",
+        "|F--J",
+        "LJ...",
+    ]
+
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
+
+    cells = advent.day_10.get_enclosed_cells(start_at, result, mx)
+
+    assert cells == {
+        Cell(2, 2),
+    }
+
+
+@pytest.mark.skip
+def test_example_02a():
+    puzzle_input = [
+        "...........",
+        ".S--------7.",
+        ".|F-----7|.",
+        ".||.....||.",
+        ".||.....||.",
+        ".|L-7.F-J|.",
+        ".|..|.|..|.",
+        ".L--J.L--J.",
+        "...........",
+    ]
+
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
+
+    cells = advent.day_10.get_enclosed_cells(start_at, result, mx)
+
+    assert len(cells) == 4
+
+    assert cells == {
+        Cell(2, 6),
+        Cell(3, 6),
+        Cell(7, 6),
+        Cell(8, 6),
+    }
+
+
+@pytest.mark.skip
+def test_example_02b():
+    puzzle_input = [
+        "..........",
+        ".S------7.",
+        ".|F----7|.",
+        ".||....||.",
+        ".||....||.",
+        ".|L-7F-J|.",
+        ".|..||..|.",
+        ".L--JL--J.",
+        "..........",
+    ]
+
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
+
+    cells = advent.day_10.get_enclosed_cells(start_at, result, mx)
+
+    assert len(cells) == 4
+
+    assert cells == {
+        Cell(2, 6),
+        Cell(3, 6),
+        Cell(6, 6),
+        Cell(7, 6),
+    }
+
+
+@pytest.mark.skip
+def test_example_02c():
+    puzzle_input = [
+        ".F----7F7F7F7F-7....",
+        ".|F--7||||||||FJ....",
+        ".||.FJ||||||||L7....",
+        "FJL7L7LJLJ||LJ.L-7..",
+        "L--J.L7...LJS7F-7L7.",
+        "....F-J..F7FJ|L7L7L7",
+        "....L7.F7||L7|.L7L7|",
+        ".....|FJLJ|FJ|F7|.LJ",
+        "....FJL-7.||.||||...",
+        "....L---J.LJ.LJLJ...",
+    ]
+
+    puzzle_output = [
+        "OF----7F7F7F7F-7OOOO",
+        "O|F--7||||||||FJOOOO",
+        "O||OFJ||||||||L7OOOO",
+        "FJL7L7LJLJ||LJIL-7OO",
+        "L--JOL7IIILJS7F-7L7O",
+        "OOOOF-JIIF7FJ|L7L7L7",
+        "OOOOL7IF7||L7|IL7L7|",
+        "OOOOO|FJLJ|FJ|F7|OLJ",
+        "OOOOFJL-7O||O||||OOO",
+        "OOOOL---JOLJOLJLJOOO",
+    ]
+
+    start_at, result, mx = advent.day_10.parse(puzzle_input)
+
+    actual = advent.day_10.get_enclosed_cells(start_at, result, mx)
+
+    assert len(actual) == 8
+    assert actual == {
+        Cell(x=6, y=6),
+        Cell(x=7, y=4),
+        Cell(x=7, y=5),
+        Cell(x=8, y=4),
+        Cell(x=8, y=5),
+        Cell(x=9, y=4),
+        Cell(x=14, y=3),
+        Cell(x=14, y=6),
+    }
+
+
+def get_answer(puzzle_output):
+    result = set()
+
+    for y, line in enumerate(puzzle_output):
+        for x, char in enumerate(line):
+            if char == "I":
+                cell = Cell(x, y)
+                result.add(cell)
+
+    return result
