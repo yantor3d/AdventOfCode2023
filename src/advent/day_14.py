68c73735776a3367a6f8980b8bde7603106891ab
Ryan Porter <ryan.m.porter@gmail.com>
2023-12-14 22:22:21 -0800

    Day 14, Part 02
    Don't ask me how long it took for me to reverse engineer the maths
    to calculate where in a cycle you are after N iterations with an offset.

--- src/advent/day_14.py
+++ src/advent/day_14.py
@@ -1,6 +1,9 @@
 """Advent of Code 2023, Day 14."""
 
+import math
 import collections
+import functools
+import itertools
 
 from typing import Dict, List, Tuple
 
@@ -10,79 +13,185 @@ Point = collections.namedtuple("Point", ("x y"))
 def part_01(puzzle_input: List[str]) -> int:
     """Solve part one."""
 
-    result = get_load(tilt(puzzle_input))
+    grid = unpack(puzzle_input)
+    grid = tilt_n(grid)
 
-    return result
+    return get_load(grid)
 
 
 def part_02(puzzle_input: List[str]) -> int:
     """Solve part two."""
 
+    grid = unpack(puzzle_input)
 
-def get_load(rocks: List[str]) -> int:
+    grids = dict()
+    loads = dict()
+
+    start, end = None, None
+    n = 1000000000
+
+    for i in range(1, n + 1):
+        grid = spin(grid)
+        loads[i] = get_load(grid)
+        
+        if grid in grids:
+            if start is None:
+                start = grids[grid]
+            
+            if end is None:
+                end = i - 1
+                break
+        else:
+            grids[grid] = i
+
+    d = end - start + 1
+    f = (n - end) // d
+
+    r = start + (n - (end + (f * d) + 1))
+
+    # Brute force answer check
+    # x = start
+    # while x < n:
+    #     x += d 
+    # s = end - (x - n) + 1
+
+    # print(s)
+    # print(r)
+
+    return loads[r]
+
+
+def get_load(grid: List[str]) -> int:
     """Return the load on the platform."""
 
     n = 0
-    m = len(rocks)
+    m = len(grid)
 
-    for i, row in enumerate(rocks):
+    for i, row in enumerate(grid):
         n += row.count("O") * (m - i)
 
     return n
 
 
-def tilt(rocks: List[str]) -> List[str]:
-    """Tilt the platform north.
-
-    All round rocks (0) move as far as they can in that direction until they
-    encounter a square rock (#), a stationary round rock, or the platform edge.
-    """
-
-    # TODO: It would be more efficient to do this by column
-
-    rocks = [list(row) for row in rocks]
-    result = _tilt(rocks)
-
-    return ["".join(row) for row in result]
+def unpack(grid: List[str]) -> List[List[str]]:
+    return tuple([tuple(row) for row in grid])
 
 
-def _tilt(rocks: List[List[str]]) -> List[List[str]]:
-    old = rocks
-    new = []
+def repack(grid: List[List[str]]) -> List[str]:
+    return [''.join(row) for row in grid]
 
-    rows = collections.deque(old)
 
-    while True:
-        a = rows.popleft()
+@functools.cache
+def spin(grid: List[List[str]]) -> List[List[str]]:
+    grid = tilt_n(grid)
+    grid = tilt_w(grid)
+    grid = tilt_s(grid)
+    grid = tilt_e(grid)
 
-        try:
-            b = rows.popleft()
-        except IndexError:
-            new.append(a)
-            break
+    return grid
+
+
+@functools.cache
+def tilt_n(grid: List[List[str]]) -> List[List[str]]:
+    grid = rotate_ccw(grid)
+    grid = roll_all(grid)
+    grid = rotate_cw(grid)
+
+    return grid
+
+
+@functools.cache
+def tilt_e(grid: List[List[str]]) -> List[List[str]]:
+    grid = mirror_h(grid)
+    grid = roll_all(grid)
+    grid = mirror_h(grid)
+
+    return grid
+
+
+@functools.cache
+def tilt_s(grid: List[List[str]]) -> List[List[str]]:
+    grid = mirror_v(grid)
+    grid = tilt_n(grid)
+    grid = mirror_v(grid)
+
+    return grid
+
+
+@functools.cache
+def tilt_w(grid: List[List[str]]) -> List[List[str]]:
+    return roll_all(grid)
+
+
+@functools.cache
+def rotate_cw(grid: List[List[str]]) -> List[List[str]]:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    return tuple([
+        tuple([grid[y][x] for y in reversed(range(num_rows))])
+        for x in range(num_cols)
+    ])
+
+
+@functools.cache
+def rotate_ccw(grid: List[List[str]]) -> List[List[str]]:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    return tuple([
+        tuple([grid[y][x] for y in range(num_rows)])
+        for x in reversed(range(num_cols))
+    ])
+
+
+@functools.cache
+def mirror_h(grid: List[List[str]]) -> List[List[str]]:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    
+    return tuple([
+        tuple([grid[y][x] for x in reversed(range(num_cols))])
+        for y in range(num_rows)
+    ])
+
+
+@functools.cache
+def mirror_v(grid: List[List[str]]) -> List[List[str]]:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    
+    return tuple([
+        tuple([grid[y][x] for x in range(num_cols)])
+        for y in reversed(range(num_rows))
+    ])
+
+
+@functools.cache
+def roll_all(grid: List[List[str]]) -> List[List[str]]:
+    return tuple([roll(each) for each in grid])
+
+
+@functools.cache
+def roll(items: List[str]):
+    empty = None
+
+    items = list(items)
+
+    for i, item in enumerate(items):
+        if item == 'O':
+            if empty is None:
+                # Can't roll any further
+                continue 
+            else:
+                items[empty], items[i] = item, '.'
+                empty += 1
+        elif item == '.':
+            empty = i if empty is None else empty 
+        elif item == '#':
+            empty = None
+            continue
         else:
-            a, b = roll(a, b)
-            new.append(a)
-            rows.appendleft(b)
+            raise RuntimeError(items)
 
-    if old == new:
-        return new
-    else:
-        return _tilt(new)
-
-
-def roll(old_p: List[List[str]], old_q: List[List[str]]) -> str:
-    """Return the result of the rocks in b rolling towards a."""
-
-    new_p = old_p[:]
-    new_q = old_q[:]
-
-    for i, (p, q) in enumerate(zip(old_p, old_q)):
-        if (p, q) == (".", "O"):
-            new_p[i] = q
-            new_q[i] = "."
-        else:
-            new_p[i] = p
-            new_q[i] = q
-
-    return new_p, new_q
+    return tuple(items)
--- tests/test_day_14.py
+++ tests/test_day_14.py
@@ -21,9 +21,108 @@ def puzzle_input():
     ]
 
 
-def test_tilt_north(puzzle_input):
-    old = puzzle_input
-    new = advent.day_14.tilt(old)
+def test_rotate():
+    old = [
+        '#.O',
+        '##.',
+        '..#'
+    ]
+
+    new = [
+        '.##',
+        '.#.',
+        '#.O',
+    ]
+
+    tmp = advent.day_14.unpack(old)
+    out = advent.day_14.rotate_cw(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == new
+
+    tmp = advent.day_14.unpack(new)
+    out = advent.day_14.rotate_ccw(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == old
+
+
+def test_mirror_h():
+    old = [
+        '#.O',
+        '##.',
+        '..#'
+    ]
+
+    new = [
+        'O.#',
+        '.##',
+        '#..',
+    ]
+
+    tmp = advent.day_14.unpack(old)
+    out = advent.day_14.mirror_h(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == new
+
+    tmp = advent.day_14.unpack(new)
+    out = advent.day_14.mirror_h(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == old
+
+
+def test_mirror_v():
+    old = [
+        '#.O',
+        '##.',
+        '..#'
+    ]
+
+    new = [
+        '..#',
+        '##.',
+        '#.O',
+    ]
+
+    tmp = advent.day_14.unpack(old)
+    out = advent.day_14.mirror_v(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == new
+
+    tmp = advent.day_14.unpack(new)
+    out = advent.day_14.mirror_v(tmp)
+    out = advent.day_14.repack(out)
+    
+    assert out == old
+
+
+@pytest.mark.parametrize(
+    'old,new',
+    (
+        ('OO.O.O..##', 'OOOO....##'),
+        ('...OO....O', 'OOO.......'),
+        ('.O...#O..O', 'O....#OO..'),
+        ('.#.O......', '.#O.......'),
+        ('#.#..O#.##', '#.#O..#.##'),
+        ('..#...O.#.', '..#O....#.'),
+        ('....O#.O#.', 'O....#O.#.'),
+        ('....#.....', '....#.....'),
+        ('.#.O.#O...', '.#O..#O...'),
+    )
+)
+def test_roll(old, new):
+    out = advent.day_14.roll(old)
+
+    assert out == tuple(new)
+
+
+def test_tilt_n(puzzle_input):
+    old = advent.day_14.unpack(puzzle_input)
+    new = advent.day_14.tilt_n(old)
+    new = advent.day_14.repack(new)
 
     assert new == [
         "OOOO.#.O..",
@@ -39,7 +138,131 @@ def test_tilt_north(puzzle_input):
     ]
 
 
+def test_tilt_e(puzzle_input):
+    old = advent.day_14.unpack(puzzle_input)
+    new = advent.day_14.tilt_e(old)
+    new = advent.day_14.repack(new)
+
+    assert new == [
+        "....O#....",
+        ".OOO#....#",
+        ".....##...",
+        ".OO#....OO",
+        "......OO#.",
+        ".O#...O#.#",
+        "....O#..OO",
+        ".........O",
+        "#....###..",
+        "#..OO#....",
+    ]
+
+
+def test_tilt_s(puzzle_input):
+    old = advent.day_14.unpack(puzzle_input)
+    new = advent.day_14.tilt_s(old)
+    new = advent.day_14.repack(new)
+
+    assert new == [
+        ".....#....",
+        "....#....#",
+        "...O.##...",
+        "...#......",
+        "O.O....O#O",
+        "O.#..O.#.#",
+        "O....#....",
+        "OO....OO..",
+        "#OO..###..",
+        "#OO.O#...O",
+    ]
+
+
+def test_tilt_w(puzzle_input):
+    old = advent.day_14.unpack(puzzle_input)
+    new = advent.day_14.tilt_w(old)
+    new = advent.day_14.repack(new)
+
+    assert new == [
+        "O....#....",
+        "OOO.#....#",
+        ".....##...",
+        "OO.#OO....",
+        "OO......#.",
+        "O.#O...#.#",
+        "O....#OO..",
+        "O.........",
+        "#....###..",
+        "#OO..#....",
+    ]
+
+
 def test_part_01(puzzle_input):
     answer = advent.day_14.part_01(puzzle_input)
 
     assert answer == 136
+    
+
+def test_spin_1(puzzle_input):
+    grid = advent.day_14.unpack(puzzle_input)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.repack(grid)
+
+    assert grid == [
+        '.....#....',
+        '....#...O#',
+        '...OO##...',
+        '.OO#......',
+        '.....OOO#.',
+        '.O#...O#.#',
+        '....O#....',
+        '......OOOO',
+        '#...O###..',
+        '#..OO#....',
+    ]
+
+    
+
+def test_spin_2(puzzle_input):
+    grid = advent.day_14.unpack(puzzle_input)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.repack(grid)
+
+    assert grid == [
+        '.....#....',
+        '....#...O#',
+        '.....##...',
+        '..O#......',
+        '.....OOO#.',
+        '.O#...O#.#',
+        '....O#...O',
+        '.......OOO',
+        '#..OO###..',
+        '#.OOO#...O',
+    ]
+
+    
+def test_spin_3(puzzle_input):
+    grid = advent.day_14.unpack(puzzle_input)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.spin(grid)
+    grid = advent.day_14.repack(grid)
+
+    assert grid == [
+        '.....#....',
+        '....#...O#',
+        '.....##...',
+        '..O#......',
+        '.....OOO#.',
+        '.O#...O#.#',
+        '....O#...O',
+        '.......OOO',
+        '#...O###.O',
+        '#.OOO#...O',
+    ]
+
+
+def test_part_02(puzzle_input):
+    answer = advent.day_14.part_02(puzzle_input)
+
+    assert answer == 64
