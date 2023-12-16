d838664d824886992316088f6af890f9c41b2273
Ryan Porter <ryan.m.porter@gmail.com>
2023-12-16 14:54:18 -0800

    Add support for day lists, ranges

--- src/advent/__main__.py
+++ src/advent/__main__.py
@@ -11,24 +11,38 @@ def main():
     """Solve the puzzle for the given day."""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("days", type=int, nargs="*")
+    parser.add_argument("days", type=str, nargs="*")
 
     args = parser.parse_args()
 
     st = time.time()
 
+    days = []
+
     for day in args.days:
+        if "-" in day:
+            mn, mx = day.split("-")
+            days.extend(range(int(mn), int(mx) + 1))
+        elif "," in day:
+            days.extend((int(d) for d in day.split))
+        else:
+            days.append(int(d))
+
+    for day in days:
         day_name = "day_{:02d}".format(day)
 
-        module = importlib.import_module(day_name, advent)
+        try:
+            module = importlib.import_module(day_name, advent)
+        except ModuleNotFoundError:
+            break
 
         print(f"\nDay {day:02d}")
         advent.solve(day, getattr(module, "part_01", None), getattr(module, "part_02", None))
 
     et = time.time()
 
-    if len(args.days) > 1:
-        print(f"\nSolved {len(args.days)} puzzles in {et - st:.2f} seconds.")
+    if len(days) > 1:
+        print(f"\nSolved {len(days)} puzzles in {et - st:.2f} seconds.")
 
 
 if __name__ == "__main__":
